79bc54afe58ff4da4b0bdf445cbe65b3fd0de070
Muhammad Saadan <muhammad.saadan@ucalgary.ca>
2019-09-17 12:36:16 -0600

    Boilerplates

---
+++ CPSC575/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CPSC575
+//
+//  Created by Muhammad Saadan on 9/17/19.
+//  Copyright © 2019 Muhammad Saadan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
