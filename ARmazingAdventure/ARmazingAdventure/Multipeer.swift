e916a0e0d2bde3c23b16dbe17ff3f5583f73046b
hanum-lee <chococloudy@gmail.com>
2019-11-19 23:30:55 -0700

    basic multiplayer class added

---
+++ ARmazingAdventure/ARmazingAdventure/Multipeer.swift
@@ -0,0 +1,100 @@
+//
+//  MultiplayerSession.swift
+//  ARmazingAdventure
+//
+//  Created by Hanum Lee on 2019-11-19.
+//  Copyright © 2019 Muhammad Saadan. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+class Multipeer: NSObject {
+
+    private let myPeerID = MCPeerID(displayName: UIDevice.current.name)
+    static let serviceType = "ar-multi-sample"
+    private var session: MCSession!
+    private var serviceAdvertiser: MCNearbyServiceAdvertiser!
+    private var serviceBrowser: MCNearbyServiceBrowser!
+    
+    private let receivedDataHandler: (Data, MCPeerID) -> Void
+    
+    init(receivedDataHandler: @escaping (Data, MCPeerID) -> Void ) {
+        self.receivedDataHandler = receivedDataHandler
+        
+        super.init()
+        
+        session = MCSession(peer: myPeerID, securityIdentity: nil, encryptionPreference: .required)
+        session.delegate = self
+        
+        serviceAdvertiser = MCNearbyServiceAdvertiser(peer: myPeerID, discoveryInfo: nil, serviceType: Multipeer.serviceType)
+        serviceAdvertiser.delegate = self
+        serviceAdvertiser.startAdvertisingPeer()
+        
+        serviceBrowser = MCNearbyServiceBrowser(peer: myPeerID, serviceType: Multipeer.serviceType)
+        serviceBrowser.delegate = self
+        serviceBrowser.startBrowsingForPeers()
+    }
+    
+    func sendToAllPeers(_ data: Data) {
+        do {
+            try session.send(data, toPeers: session.connectedPeers, with: .reliable)
+        } catch {
+            print("error sending data to peers: \(error.localizedDescription)")
+        }
+    }
+    
+    var connectedPeers: [MCPeerID] {
+        return session.connectedPeers
+    }
+    
+
+}
+
+extension Multipeer: MCSessionDelegate {
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        // not used
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        receivedDataHandler(data, peerID)
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        fatalError("This service does not send/receive streams.")
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        fatalError("This service does not send/receive resources.")
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        fatalError("This service does not send/receive resources.")
+    }
+    
+}
+
+extension Multipeer: MCNearbyServiceBrowserDelegate {
+    
+    /// - Tag: FoundPeer
+    public func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String: String]?) {
+        // Invite the new peer to the session.
+        browser.invitePeer(peerID, to: session, withContext: nil, timeout: 10)
+    }
+
+    public func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        // This app doesn't do anything with non-invited peers, so there's nothing to do here.
+    }
+    
+}
+
+extension Multipeer: MCNearbyServiceAdvertiserDelegate {
+    
+    /// - Tag: AcceptInvite
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        // Call handler to accept invitation and join the session.
+        invitationHandler(true, self.session)
+    }
+
+}
